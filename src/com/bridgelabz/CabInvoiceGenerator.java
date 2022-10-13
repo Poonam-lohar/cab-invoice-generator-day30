1cd453dc5c4d4efdb59b6df81b515701a11c0300
Your Name <you@example.com>
2022-10-13 10:31:40 +0530

    added:logic for invoice service

--- src/com/bridgelabz/CabInvoiceGenerator.java
+++ src/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,6 +1,12 @@
 package com.bridgelabz;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CabInvoiceGenerator {
+    /**
+     * final variable
+     */
     private static final int COST_PER_KM = 10;
     private static final int COST_PER_MIN = 1;
     private static final int MIN_FARE = 5;
@@ -36,7 +42,6 @@ public class CabInvoiceGenerator {
     /**
      * Creating a parameterized method name as calculateFareForMultipleRides
      * Take a multiple ride and calculate the aggregate total for all
-     *
      * @param rides multiple ride
      * @return total fare
      */
@@ -56,21 +61,74 @@ public class CabInvoiceGenerator {
     }
 
     /**
-     * @param rides
-     * @return
+     * create a method name as invoiceSummaryCalculation
+     * @param rides total no of rides
+     * @return invoice summaray calculation return total ride
      */
     public InvoiceSummery invoiceSummaryCalculation(Ride[] rides) {
+        //variable
         double totalFare = 0.0;
+        /**
+         * using for each loop for total fare
+         */
         for (Ride ride : rides) {
             totalFare += calculateFare(ride.getDistance(), ride.getTime());
         }
+        /**
+         * return total invoice summary
+         */
         return new InvoiceSummery(rides.length, totalFare);
+    }
+
+    /**
+     * create a method name as getInvoice
+     * @param userId given user id
+     * @return tatal rides array
+     */
+    public InvoiceSummery getInvoice(int userId) {
+        /**
+         * crete a map object
+         */
+        Map<Integer, Ride[]> map = new HashMap<>();
+        /**
+         * 1st ride
+         */
+        Ride[] rides1 = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        /**
+         * 2nd ride
+         */
+        Ride[] rides2 = {new Ride(5.0, 10),
+                new Ride(1, 1)
+        };
+
+        /**
+         * 3rd ride
+         */
+        Ride[] rides3 = {new Ride(8.0, 15),
+                new Ride(1, 10)
+        };
+        /**
+         * calling put method from map object
+         */
+        map.put(1, rides1);
+        map.put(2, rides2);
+        map.put(3, rides3);
+
+        for (Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
+            if (userId == entry.getKey()) {
+                System.out.println(entry.getKey());
+                Ride[] ridesArray = entry.getValue();
+                return invoiceSummaryCalculation(ridesArray);
+            }
+        }
+        return null;
     }
 
 
     /**
      * create a main method ,all program execute in main method
-     *
      * @param args no arguments
      */
     public static void main(String[] args) {
@@ -83,5 +141,18 @@ public class CabInvoiceGenerator {
          * and print o/p
          */
         System.out.println(cabInvoiceGenerator.calculateFare(5, 20) + " Rs");
+
+        /**
+         * rides array = distance,time
+         */
+        Ride[] ridesArray = {
+                new Ride(0.1,1),
+                new Ride(1,1),
+                new Ride(1,10)
+        };
+        /**
+         * calling getInvoice method from cabInvoiceGenerator object
+         */
+        cabInvoiceGenerator.getInvoice(1);
     }
-}
+   }
--- src/com/bridgelabz/Ride.java
+++ src/com/bridgelabz/Ride.java
@@ -1,9 +1,25 @@
 package com.bridgelabz;
 
 public class Ride {
+    /**
+     * private variables can only be accessed within the same class (an outside class has no access to it)
+     * private = restricted access
+     * However, it is possible to access them if we provide public get and set methods.
+     */
+
+
+    /**
+     * final variable
+     */
     private final double distance;
     private final int time;
 
+    /**
+     * create constructor name as Ride,this is parameterized constructor
+     *
+     * @param distance in cab invoice generator
+     * @param time     in cab invoice generator
+     */
     public Ride(double distance, int time) {
 
         /**
@@ -13,12 +29,23 @@ public class Ride {
         this.time = time;
     }
 
+    /**
+     * crete a get method name as getDistance
+     * The get method returns the value of the variable
+     *
+     * @return distance
+     */
     public double getDistance() {
         return distance;
     }
 
+    /**
+     * crete a get method name as getTime
+     * The get method returns the value of the variable
+     *
+     * @return time
+     */
     public int getTime() {
         return time;
-
     }
 }
